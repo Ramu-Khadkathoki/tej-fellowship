7b0c6529c3c7aa83f44a9eaf48ff61133af5c8e1
Ramu-Khadkathoki <101965375+Ramu-Khadkathoki@users.noreply.github.com>
2022-04-13 01:11:07 +0545

    Create scripts.js

---
+++ css/scripts.js
@@ -0,0 +1,18 @@
+//function to load at start
+function load()
+{
+	alert("Hello TEJ!");
+}
+
+//function for button
+function sayHello()
+{
+	alert("Hello there!");
+}
+// for increment and decrement
+function increment() {
+      document.getElementById('myInput').stepUp();
+   }
+   function decrement() {
+      document.getElementById('myInput').stepDown();
+   }
